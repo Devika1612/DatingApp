1f85035cd88410b774b0852f50bf0f4125a15438
Devika <devikasiddani@gmail.com>
2023-05-10 08:10:28 +0530

    RoutingChanges

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,28 +14,24 @@ export class NavComponent implements OnInit{
   model:any={};
   currentUser$:Observable<User|null>=of(null);
   // loggedIn=false;
-  constructor(public accountService:AccountService){}
+  constructor(public accountService:AccountService,private router:Router,
+    private toastr:ToastrService){}
   ngOnInit(): void {
-  //  this.currentUser$=this.accountService.currentUser$;
+
   }
 
-  // getCurrentUser(){
-  //   this.accountService.currentUser$.subscribe({
-  //     next:user=>this.loggedIn=!user,
-  //     error:error=>console.log(error)
-  //   })
-  // }
+  
   login(){
     this.accountService.login(this.model).subscribe({
-      next:response=>{
-        console.log(response);
+      next:_=>{
+        this.router.navigateByUrl('/members')
       },
-      error:error=>console.log(error)
+      error:error=>this.toastr.error(error.error)
     })
     console.log(this.model);
   }
   logout(){
-    this.accountService.logout();
+    this.accountService.logout();this.router.navigateByUrl('/');
   }
 
 }
